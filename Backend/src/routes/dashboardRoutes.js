978c3403e90ceb1cee86f69728d520f69fd1a140
vivekkshar <vksharma7303@gmail.com>
2026-08-04 16:11:14 +0530

    sixth commit

--- Backend/src/routes/dashboardRoutes.js
+++ Backend/src/routes/dashboardRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
-import { isAuthenticated } from "../middleware/authMiddleware.js";
-import { authorizeRoles } from "../middleware/roleMiddleware.js";
+import { isAuthenticated } from "../middleware/authmiddleware.js";
+import { authorizeRoles } from "../middleware/rolemiddleware.js";
 import  { getRecruiterDashboard }  from "../controllers/dashboardController.js";
 
 const router = express.Router();
