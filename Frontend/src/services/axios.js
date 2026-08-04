2a560a0687c7f54defc4fce855fdbd7de9efa42d
vivekkshar <vksharma7303@gmail.com>
2026-08-04 16:16:12 +0530

    seventh commit

--- Frontend/src/services/axios.js
+++ Frontend/src/services/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://job-portal-1-1siw.onrender.com/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
